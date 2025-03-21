f3a270518861bcc0bca94d819ff543c672ee3199
anastasiamv2007 <anastasiamelnychyn@gmail.com>
2025-03-21 11:17:31 +0200

    add class film realization

--- +-/Film.cpp
+++ +-/Film.cpp
@@ -1,1 +1,47 @@
 #include "Film.h"
+
+Film::Film():f_title(" "), f_country(" "), f_duration(0.0)
+{
+
+}
+
+Film::Film(string title, string country, double duration):f_title(title), f_country(country), f_duration(duration)
+{
+}
+
+Film::Film(const Film& F):f_title(F.f_title), f_country(F.f_country), f_duration(F.f_duration)
+{
+}
+
+Film::~Film()
+{
+}
+
+string Film::getDurationInHours() const
+{
+	return string();
+}
+
+Film Film::operator-(int minutes) const
+{
+	return Film();
+}
+
+void Film::print() const
+{
+}
+
+string Film::getTitle()
+{
+	return string();
+}
+
+string Film::getCountry()
+{
+	return string();
+}
+
+int Film::getDuration()
+{
+	return 0;
+}
--- +-/Film.h
+++ +-/Film.h
@@ -6,15 +6,23 @@ using namespace std;
 class Film
 {
 protected:
-	string title;
-	string country;
-	double duration;
+	string f_title;
+	string f_country;
+	double f_duration;
 public:
 	Film();
+	Film(string title, string country, double duration);
 	Film(const Film& F);
 	virtual ~Film();
+
+	string getDurationInHours()const;
     Film operator-(int minutes) const ;
 	virtual void print() const;
+
+	string getTitle();
+	string getCountry();
+	int getDuration();
+
 
 
 
