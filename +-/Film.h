25334be54c9861209f082a57e00af4915805eb4c
anastasiamv2007 <anastasiamelnychyn@gmail.com>
2025-03-21 10:52:05 +0200

    add new class

---
+++ +-/Film.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include<string>
+using namespace std;
+
+class Film
+{
+protected:
+	string title;
+	string country;
+	double duration;
+public:
+	Film();
+	Film(const Film& F);
+	virtual ~Film();
+	Film operator-();
+
+
+
+
+};
+
