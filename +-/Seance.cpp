65c97cdf144630e91fbccfd5bbfdfdfc7070ea04
Batyuchka <martabatuk1328@gmail.com>
2025-03-21 11:31:41 +0200

    -----

--- +-/Seance.cpp
+++ +-/Seance.cpp
@@ -1,23 +1,24 @@
 #include "Seance.h"
 
 Seance::Seance(string t, string c, double d, string da, double st)
-{
-}
+	: Film(t,c,d), day(da) , start_seance(st)
+{}
 
 int Seance::getEndTime() const
 {
-	return 0;
+	return start_seance + f_duration;
 }
 
-void Seance::moveToNextDay()
-{
-}
+//void Seance::moveToNextDay()
+//{
+//	static string days[] = { "Monday","Tuesday" ,"Wednesday","Thursday" ,"Friday" ,"Saturday"}
+//}
 
-void Seance::printOn() const
-{
-}
+//void Seance::printOn() const
+//{
+//}
 
-bool Seance::operator<(const Seance& S) const
-{
-	return false;
-}
+//bool Seance::operator<(const Seance& S) const
+//{
+//	return false;
+//}
